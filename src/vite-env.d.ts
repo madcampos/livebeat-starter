a6f85e0a7fec24e97b70505ceeb32e6a356f5f8e
madcampos <madcampos@outlook.com>
2024-02-11 02:45:18 -0500

    feat: add env vars

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,19 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+	/** The app mode. Can be either `development` or `production`. */
+	readonly MODE: 'development' | 'production',
+	readonly PROD: boolean,
+	readonly DEV: boolean,
+
+	readonly VITE_APPWRITE_ENDPOINT: string,
+	readonly VITE_APPWRITE_PROJECT: string,
+}
+
+interface ImportMeta {
+	hot: {
+		accept: Function,
+		dispose: Function
+	},
+	readonly env: ImportMetaEnv
+}
